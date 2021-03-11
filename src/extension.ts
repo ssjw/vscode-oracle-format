8040cebe2d55efb37958c62686c4e9debc2c2ac8
Jonathan Wheaton <jonathan@wheaton.space>
2021-03-10 22:06:21 -0500

    Added option to use Trivadis Formatter

--- src/extension.ts
+++ src/extension.ts
@@ -10,18 +10,31 @@ const execPromise = util.promisify(exec);
 
 function parseOutputForErrors(input: string) {
   // Catch "parse error"
-  if (input.search(/parse error/i) !== -1) {
+  if (input.search(/parse error/i) !== -1 ||
+    input.search(/Syntax Error/) !== -1) {
     throw Error("Skipped formatting: parse error");
   }
   // Catch error loading rules
   if (input.search(/cannot be opened/i) !== -1) {
     throw Error("Formating rules cannot be loaded");
   }
+  // Can't find Trivadis JS script (probably)
+  if (input.search(/Could not load file/) !== -1) {
+    throw Error("Trivadis format Javascript file not found.");
+  }
+  // Catch errors reading files in Trivadis format.js.
+  if (input.search(/does not exist/i) !== -1) {
+    throw Error("File not found error in Trivadis format script");
+  }
   // Catch any unknow "error"
   const reg = /^.*error.*$/gim;
   const s = reg.exec(input);
   if (s !== null) {
     throw Error(s[0]);
+  }
+  // Catch warnings from Trivadis format.js
+  if (input.search(/Warning/) !== -1) {
+    vscode.window.showWarningMessage("Warning found in format script output.  Check the oracle-format output channel.");
   }
 }
 
@@ -40,6 +53,17 @@ export function activate(context: vscode.ExtensionContext) {
 
       const sqlPathConfig: string = getSettings().get("sqlcl");
       const rulesPathConfig: string = getSettings().get("rules");
+      const useTrivadis: boolean = getSettings().get("useTrivadisFormatter");
+      const trivadisFormatScript: string = getSettings().get("trivadisFormatScript");
+      if (useTrivadis && (
+        trivadisFormatScript === "" || trivadisFormatScript === null ||
+        trivadisFormatScript === undefined)) {
+        throw Error("Use Trivadis Formatter setting is true, but Trivadis Format Script is not set.");
+      }
+      const trivadisXml: string = getSettings().get("trivadisXml") ?
+        `xml=` + getSettings().get("trivadisXml") : "";
+      const trivadisArbori: string = getSettings().get("trivadisArbori") ?
+        `arbori=` + getSettings().get("trivadisArbori") : "";
 
       // Substutite ${workspaceFolder} variable in configuration paths
       const workspaceFolder = vscode.workspace.workspaceFolders
@@ -65,6 +89,11 @@ exit`
         : `set echo on
 format file "${tempFile}" "${tempFile}"
 exit`;
+
+      const cmdScriptTrivadis = `set echo on
+script ${trivadisFormatScript} "${tempFile}" ${trivadisXml} ${trivadisArbori}
+exit`;
+
       const formatScript = join(storagePath, "format.sql");
 
       // Cmd command to execute script file with sqlcl
@@ -77,7 +106,7 @@ exit`;
         if (!existsSync(storagePath)) {
           mkdirSync(storagePath);
         }
-        writeFileSync(formatScript, cmdScript);
+        writeFileSync(formatScript, useTrivadis ? cmdScriptTrivadis : cmdScript);
         writeFileSync(tempFile, document.getText(range));
 
         // Execute formating with SqlCl on temp file
